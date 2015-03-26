f9503eadc8659e1779db79efccf76805dc1364c2
Justin Ball <justinball@gmail.com>
2015-03-26 13:06:46 -0600

    Redirect to '/' after OAuth

--- app/utils/strategy_common.js
+++ app/utils/strategy_common.js
@@ -4,8 +4,15 @@ module.exports = {
     passport.authenticate(kind, function(err, user, info) {
       if (err) { return next(err); }
       if (!user) { return res.status(422).json(info); } // HTTP status 422: user create failed
-      // User Registration was successful send back the user
-      res.json({"user": req.user});
+      
+      if(kind == 'local-login'){
+        // User Registration was successful send back the user
+        res.json({"user": req.user});
+      } else {
+        // OAuth login. Redirect to home page.
+        res.redirect('/');
+      }
+
     })(req, res, next);
   }
 
