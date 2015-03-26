7e4bd6271aeda48c3b843146598bdd830630c407
Justin Ball <justinball@gmail.com>
2015-03-25 22:11:14 -0600

    Added notes

--- config/secrets.example.js
+++ config/secrets.example.js
@@ -4,13 +4,13 @@ module.exports = {
 
   'sessionSecret' : 'Generate a secure session secret and put it here.',
 
-  'facebookAuth' : {
+  'facebookAuth' : { // Get this from https://developers.facebook.com
     'clientID'    : 'your-secret-clientID-here', // your App ID
     'clientSecret'  : 'your-client-secret-here', // your App Secret
     'callbackURL'   : 'http://localhost:8080/auth/facebook/callback' // In production localhost will need to be your live url
   },
 
-  'twitterAuth' : {
+  'twitterAuth' : { // Get this from https://apps.twitter.com/
     'consumerKey'     : 'your-consumer-key-here',
     'consumerSecret'  : 'your-client-secret-here',
     'callbackURL'     : 'http://localhost:8080/auth/twitter/callback'
