27225efddf2c711c58f15c70b25d518c71e586cc
Justin Ball <justinball@gmail.com>
2015-03-27 13:33:29 -0600

    Formatted code

--- app/utils/strategy_common.js
+++ app/utils/strategy_common.js
@@ -2,8 +2,12 @@ module.exports = {
 
   finishAuth: function(kind, passport, req, res, next){
     passport.authenticate(kind, function(err, user, info) {
-      if (err) { return next(err); }
-      if (!user) { return res.status(422).json(info); } // HTTP status 422: user create failed
+      
+      if(err){ return next(err); }
+
+      if(!user){  // HTTP status 422: user create failed
+        return res.status(422).json(info);
+      } 
       
       if(kind == 'local-login'){
         // User Registration was successful send back the user
