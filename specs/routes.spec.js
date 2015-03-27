cfdcc0ce42a588a2dc160e30d0add22d0dd49423
dittonjs <jditton.atomic@gmail.com>
2015-03-27 13:43:41 -0600

    added routes test for the other routes

--- specs/routes.spec.js
+++ specs/routes.spec.js
@@ -24,3 +24,53 @@ describe('about route', function () {
     });
   });
 });
+
+describe('login route', function(){
+  it('renders login', function (done) {
+    Router.run(routes, '/login', function  (Handler, state) {
+      var html = React.renderToString(<Handler/>);
+      expect(html).toMatch(/Login/);
+      done();
+    });
+  });
+});
+
+describe('register route', function(){
+  it('renders register', function (done) {
+    Router.run(routes, '/register', function  (Handler, state) {
+      var html = React.renderToString(<Handler/>);
+      expect(html).toMatch(/Signup/);
+      done();
+    });
+  });
+});
+
+describe('logout route', function(){
+  it('renders logout', function (done) {
+    Router.run(routes, '/logout', function  (Handler, state) {
+      var html = React.renderToString(<Handler/>);
+      expect(html).toMatch(/Logout/);
+      done();
+    });
+  });
+});
+
+describe('dashboard route', function(){
+  it('renders dashboard', function (done) {
+    Router.run(routes, '/dashboard', function  (Handler, state) {
+      var html = React.renderToString(<Handler/>);
+      expect(html).toMatch(/Dashboard/);
+      done();
+    });
+  });
+});
+
+describe('connections route', function(){
+  it('renders connections', function (done) {
+    Router.run(routes, '/connections', function  (Handler, state) {
+      var html = React.renderToString(<Handler/>);
+      expect(html).toMatch(/Connections/);
+      done();
+    });
+  });
+});
